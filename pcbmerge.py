d24ac751509f7e14932f53e9a6449984e55f0f56
László Monda <laci@monda.hu>
2023-04-16 13:07:45 +0200

    Update script to the KiCad 7 API.

--- pcbmerge.py
+++ pcbmerge.py
@@ -4,7 +4,7 @@
 # Author: Florian Kauer <florian.kauer@koalo.de>
 #
 # Do not start this directly, instead use a merge script, for example
-# 
+#
 # from pcbnew import *
 # import pcbmerge
 #
@@ -13,7 +13,7 @@
 #                base_anchor = "OUTPUT",
 #                addon_anchor = "IN",
 #                filename = "example_led/led.kicad_pcb")
-# 
+#
 # pcbmerge.combine_all_areas(mypcb)
 # pcbmerge.fill_all_areas(mypcb)
 #
@@ -22,12 +22,12 @@
 #########################################################################
 #
 # This is free and unencumbered software released into the public domain.
-# 
+#
 # Anyone is free to copy, modify, publish, use, compile, sell, or
 # distribute this software, either in source code form or as a compiled
 # binary, for any purpose, commercial or non-commercial, and by any
 # means.
-# 
+#
 # In jurisdictions that recognize copyright laws, the author or authors
 # of this software dedicate any and all copyright interest in the
 # software to the public domain. We make this dedication for the benefit
@@ -35,7 +35,7 @@
 # successors. We intend this dedication to be an overt act of
 # relinquishment in perpetuity of all present and future rights to this
 # software under copyright law.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -43,18 +43,17 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-# 
+#
 # For more information, please refer to <http://unlicense.org>
 
 from collections import namedtuple
 from pcbnew import *
-import inspect
 import tempfile
 import os
 from contextlib import contextmanager
 
 def find_module_by_value(pcb, value):
-    modules = filter(lambda m: m.GetValue() == value, pcb.GetModules())
+    modules = list(filter(lambda m: m.GetValue() == value, pcb.GetFootprints()))
     assert len(modules) > 0, "%s not found" % value
     assert len(modules) == 1, "%s is not unique" % value
     return modules[0]
@@ -63,9 +62,9 @@ Displacement = namedtuple("Displacement", "delta anchor rot")
 
 def calculate_displacement(a, b):
     return Displacement(
-            delta = a.GetPosition() - b.GetPosition(),
+            delta = b.GetPosition() - a.GetPosition(),
             anchor = a.GetPosition(),
-            rot = a.GetOrientation() - b.GetOrientation())
+            rot = b.GetOrientation() - a.GetOrientation())
 
 def move(element, displacement):
     element.Move(displacement.delta)
@@ -105,10 +104,9 @@ def merge(pcb, base_anchor, addon_anchor, filename, postfix = "-ADDON"):
     displacement = calculate_displacement(base_anchor_module, addon_anchor_module)
 
     # Remember existing elements in base
-    for t in pcb.GetTracks():
-        t.SetFlags(FLAG0)
-    module = pcb.GetModules().GetLast()
-    drawing = pcb.GetDrawings().GetLast()
+    tracks = pcb.GetTracks()
+    footprints = pcb.GetFootprints()
+    drawings = pcb.GetDrawings()
     zonescount = pcb.GetAreaCount()
 
     # Determine new net names
@@ -116,7 +114,7 @@ def merge(pcb, base_anchor, addon_anchor, filename, postfix = "-ADDON"):
     for i in range(1, pcb_tmp.GetNetCount()): # 0 is unconnected net
         name = pcb_tmp.FindNet(i).GetNetname()
         new_netnames[name] = name+postfix
-    
+
     for (a,b) in zip(base_anchor_module.Pads(), addon_anchor_module.Pads()):
         new_netnames[b.GetNet().GetNetname()] = a.GetNet().GetNetname()
 
@@ -132,40 +130,25 @@ def merge(pcb, base_anchor, addon_anchor, filename, postfix = "-ADDON"):
         with open(fname) as fp:
             pcbtext = fp.read()
 
-        for (old,new) in new_netnames.iteritems():
+        for (old,new) in new_netnames.items():
             pcbtext = pcbtext.replace(old,new)
-        
+
         with open(fname,'w') as fp:
             fp.write(pcbtext)
 
         # Append new board file with modified net names
-        plugin = IO_MGR.PluginFind(IO_MGR.KICAD)
+        plugin = IO_MGR.PluginFind(IO_MGR.KICAD_SEXP)
         plugin.Load(fname, pcb)
 
     # Move
-    for t in pcb.GetTracks():
-        if t.GetFlags() & FLAG0:
-            t.ClearFlags(FLAG0)
-            continue
-        move(t, displacement)
-        
-    if module:
-        module = module.Next()
-    else:
-        module = pcb.GetModules().GetFirst()
+    for track in tracks:
+        move(track, displacement)
 
-    while module:
-        move(module, displacement)
-        module = module.Next()
+    for footprint in footprints:
+        move(footprint, displacement)
 
-    if drawing:
-        drawing = drawing.Next()
-    else:
-        drawing = pcb.GetDrawings().GetFirst()
-
-    while drawing:
+    for drawing in drawings:
         move(drawing, displacement)
-        drawing = drawing.Next()
 
-    for i in range(zonescount, pcb.GetAreaCount()):
+    for i in range(0, zonescount):
         move(pcb.GetArea(i), displacement)
